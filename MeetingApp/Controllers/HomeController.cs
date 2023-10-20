6540f3f40d7e683619d74abb6bb1314b2fc8a5e0
Amo <Amo@DESKTOP-3OS813I>
2023-10-20 04:43:55 +0300

    Add project files.

---
+++ MeetingApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			int saat = DateTime.Now.Hour;
+			ViewBag.selamlama = saat > 12 ? "İyi Günler" : "Günaydın";
+
+
+			var meetingInfo = new MeetingInfo()
+			{
+				Id = 1,
+				Location = "İstanbul, Kongre Merkezi",
+				Date = new DateTime(2024, 01, 21, 20, 0, 0),
+				NumberOfPeople = 100
+			};
+
+			return View(meetingInfo);
+		}
+	}
+}
