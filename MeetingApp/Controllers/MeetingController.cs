662ea227cbef45bfdf366a54ae17d9c5520297c7
Ahmet Bogar <ahmetbogar@hotmail.com>
2023-10-21 17:27:12 +0300

    21.10.23

--- MeetingApp/Controllers/MeetingController.cs
+++ MeetingApp/Controllers/MeetingController.cs
@@ -3,31 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp.Controllers
 {
-    public class MeetingController : Controller
-    {
-        [HttpGet]
-        public IActionResult Apply()
-        {
-            return View();
-        }
+	public class MeetingController : Controller
+	{
+		[HttpGet]
+		public IActionResult Apply()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public IActionResult Apply(UserInfo model)
 		{
-            Repository.CreateUser(model);
-            ViewBag.UserCount=Repository.Users.Where(x=>x.WillAttend ==true).Count();
-			return View("Thanks",model);
+			if (ModelState.IsValid)
+			{
+				Repository.CreateUser(model);
+				ViewBag.UserCount = Repository.Users.Where(x => x.WillAttend == true).Count();
+				return View("Thanks", model);
+			}
+			else
+			{
+				return View(model);
+			}
+
 		}
 
 		public IActionResult List()
-        {
-            var users = Repository.Users; 
-            return View(users);
-        }
+		{
+			var users = Repository.Users;
+			return View(users);
+		}
 
-        public IActionResult Details(int id)
-        {
-            return View(Repository.GetById(id));
-        }
-    }
+		public IActionResult Details(int id)
+		{
+			return View(Repository.GetById(id));
+		}
+	}
 }
--- MeetingApp/Models/UserInfo.cs
+++ MeetingApp/Models/UserInfo.cs
@@ -1,11 +1,22 @@
-﻿namespace MeetingApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetingApp.Models
 {
 	public class UserInfo
 	{
         public int Id { get; set; }
+        
+		[Required(ErrorMessage ="İsim bilgisi giriniz.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Telefon bilgisi giriniz.")]
         public string? Phone { get; set; }
-        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Mail adres bilgisi giriniz.")]
+        [EmailAddress]
+		public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Katılım durumunuzu seçiniz.")]
         public bool? WillAttend { get; set; }
     }
 }
